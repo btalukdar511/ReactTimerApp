737ecfc46ec66443a446b451c488d9dfcaba4224
xCalifier <xCalifier@gmail.com>
2017-12-15 02:44:49 +0530

    Timer functionality added

--- app/tests/components/Stopwatch.test.jsx
+++ app/tests/components/Stopwatch.test.jsx
@@ -13,12 +13,27 @@ describe('Stopwatch', ()=>{
    });
 
    describe('handleSetCountdown', ()=>{
-      it('should set state to started and countdown',()=>{
+      it('should set state to started and countdown',(done)=>{
           var countdown = TestUtils.renderIntoDocument(<Stopwatch/>);
           countdown.handleSetTimer(10);
 
           expect(countdown.state.count).toBe(10);
           expect(countdown.state.countdownStatus).toBe('started');
+
+          setTimeout(()=>{
+              expect(countdown.state.count).toBe(9);
+              done();
+          }, 1001)
       });
+
+       it('should not go down zero',(done)=>{
+           var countdown = TestUtils.renderIntoDocument(<Stopwatch/>);
+           countdown.handleSetTimer(2);
+
+           setTimeout(()=>{
+               expect(countdown.state.count).toBe(0);
+               done();
+           }, 4001)
+       });
    });
 });
